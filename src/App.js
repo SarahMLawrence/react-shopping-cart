2e388a4d1a86b0bfefad048e9a2582f9c3b0afc0
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-06-24 22:17:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,26 +7,33 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import { ProductContext } from './contexts';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products  />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+
+			</ProductContext.Provider>
 		</div>
 	);
 }
